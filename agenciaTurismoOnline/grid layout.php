216e15b81a3614159bf5afbe954e7e9f68ee49c9
absolutebr <87660259+absolutebr@users.noreply.github.com>
2021-09-16 17:24:57 -0300

    Add files via upload

---
+++ agenciaTurismoOnline/grid layout.php
@@ -0,0 +1,44 @@
+    <!DOCTYPE html>
+    <html lang="br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Viaje,</title>
+        <link href="_css/estilo-grid-layout.css" rel="stylesheet">
+        
+  </head>
+    <body>
+
+
+  <header>
+    <img src="_imagens/logotipo.png">
+  </header>
+
+    
+    <div class="grid-container">
+
+    <div class="area_2">Busca</div>
+
+    <div class="area_3">Lateral Esquerda</div>
+          
+        <div class="area_4">
+            <img src="_imagens/berlin.png">
+          </div>
+          
+          <div class="area_5">
+            <img src="_imagens/melbourne.png">
+          </div>
+          
+          <div class="area_6">
+            <img src="_imagens/miami.png">
+          </div>
+        
+          <div class="area_7">
+          <img src="_imagens/toronto.png">
+          </div>
+          <div class="area_8">Rodapé</div>
+      </div>
+
+
+  </body>
+  </html>
